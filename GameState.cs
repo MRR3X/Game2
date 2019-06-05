55dfd7fccf82a4b99c30e2e340b7faa72397ad52
chen hao <chenhao.cheny@gmail.com>
2019-06-05 10:49:23 +0200

    gravity klaar

--- GameState.cs
+++ GameState.cs
@@ -14,24 +14,18 @@ namespace MyGame
       
         Player player;
         Background background;
-        Vector2 pos = new Vector2();
+      
 
         public GameState()
         {
             player = new Player(new Vector2(0, 0));
-          
             background = new Background(new Vector2(0, 0));
+            
         }
 
         public void Update(GameTime gameTime)
         {
-          /*  pos = new Vector2(player.Position.X,player.Position.Y);
-            if (Keyboard.GetState().IsKeyDown(Keys.Left))
-            {
-                
-                player1.Position = pos;
-            }*/
-
+         
             player.Update(gameTime);
             background.Update(gameTime);
 
--- Player.cs
+++ Player.cs
@@ -7,42 +7,84 @@ namespace MyGame
 {
     public class Player
     {
+        Support.Texture lleft;
+        Support.Texture rright;
+        Support.Texture up;
+        Support.Texture down;
         Support.Texture left;
         Support.Texture right;
         Support.Texture active;
+        Support.Texture downn;
 
+
+        int result = 5;
         public Player(Vector2 position)
         {
             left = new Support.Texture("Player1", position, new Vector2(0.2f, 0.2f));
             right = new Support.Texture("Player", position, new Vector2(0.2f, 0.2f));
+
+            up = new Support.Texture("up", position, new Vector2(0.2f, 0.2f));
+            rright = new Support.Texture("right", position, new Vector2(0.2f, 0.2f));
+            lleft = new Support.Texture("left", position, new Vector2(0.2f, 0.2f));
+            down = new Support.Texture("down", position, new Vector2(0.2f, 0.2f));
+            downn = new Support.Texture("downn", position, new Vector2(0.2f, 0.2f));
             active = right;
+
         }
 
         public void Update(GameTime gameTime)
         {
             Vector2 previousPos = active.Position;
-            if (Keyboard.GetState().IsKeyDown(Keys.Up)) active.Position.Y += (float)gameTime.ElapsedGameTime.TotalSeconds * 0.5f;
 
-              
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Right))
-            {
-                active.Position.X += (float)gameTime.ElapsedGameTime.TotalSeconds * 0.3f;
-                right.Position = active.Position;
-                active = right;
+
+                up.Position = active.Position;
+                active = up;
+                if (Keyboard.GetState().IsKeyDown(Keys.D))
+                { active.Position.X += (float)gameTime.ElapsedGameTime.TotalSeconds * 0.3f; rright.Position = active.Position; active = rright; }
+                if (Keyboard.GetState().IsKeyDown(Keys.Q))
+                {
+                    active.Position.X -= (float)gameTime.ElapsedGameTime.TotalSeconds * 0.3f; lleft.Position = active.Position; active = lleft;
+                }
+                if (Keyboard.GetState().IsKeyDown(Keys.Z))
+                {
+                    active.Position.Y += (float)gameTime.ElapsedGameTime.TotalSeconds * 0.3f; up.Position = active.Position; active = up;
+                }
+                if (Keyboard.GetState().IsKeyDown(Keys.S))
+                {
+                    active.Position.Y -= (float)gameTime.ElapsedGameTime.TotalSeconds * 0.2f; down.Position = active.Position; active = down;
+                }
+                if (Keyboard.GetState().IsKeyDown(Keys.S) && Keyboard.GetState().IsKeyDown(Keys.Q))
+                {
+                    active.Position.Y -= (float)gameTime.ElapsedGameTime.TotalSeconds * 0.2f; downn.Position = active.Position; active = downn;
+                }
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            else if (Keyboard.GetState().IsKeyUp(Keys.Space))
             {
-                active.Position.X -= (float)gameTime.ElapsedGameTime.TotalSeconds * 0.3f;
-                left.Position = active.Position;
-                active = left;
+                right.Position = active.Position; active = right;
+
+                if (Keyboard.GetState().IsKeyDown(Keys.D))
+                { active.Position.X += (float)gameTime.ElapsedGameTime.TotalSeconds * 0.3f; right.Position = active.Position; active = right; active.Position.Y -= 0.001f; }
+                if (Keyboard.GetState().IsKeyDown(Keys.Q))
+                { active.Position.X -= (float)gameTime.ElapsedGameTime.TotalSeconds * 0.3f; left.Position = active.Position; active = left; active.Position.Y -= 0.001f; }
             }
+
+
+            if (active == left)
+            { active.Position.Y -= (float)gameTime.ElapsedGameTime.TotalSeconds * 0.2f; left.Position = active.Position; }
+            if (active == right)
+            { active.Position.Y -= (float)gameTime.ElapsedGameTime.TotalSeconds * 0.2f; right.Position = active.Position; }
+            if (active.Position.Y < -1.5f)
+            { active.Position.Y = -1.5f; active.Position.X = previousPos.X; }
 
             var status = Support.Camera.GetCollision(active);
             if (status != Support.CollisionStatus.Inside)
             {
                 active.Position = previousPos;
             }
+
         }
 
         public void Draw()
